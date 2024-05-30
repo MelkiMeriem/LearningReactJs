74eaba9d7daa6dcd2a1dfa67759d180563b19046
MelkiMeriem <meriem.melki@insat.ucar.tn>
2024-05-30 13:37:17 +0100

    Update App.jsx
    get request example

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,31 @@
 import {UserProfile} from './Components/UserProfile.jsx';
 import './globals.css'
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {LoginForm} from "./Components/LoginForm.jsx";
 import {RegisterForm} from "./Components/RegisterForm.jsx";
 export default function App(){
-    const [counter,setCounter]=useState(3);
-    const [username,setUsername]=useState("");
-    const[email,setEmail]=useState("");
-    const [users,setUsers]=useState([{
-        id:1,
-        username:"Mariem",
-        email : "melkimariem150@gmail.com"},
-        {
-            id:2,
-            username: "Zaineb",
-            email: "melkizaineb150@gmail.com"
-        }
-    ]);
-
-
+    const [counter,setCounter]=useState(0);
+    useEffect(() => {
+        console.log("rendering...")
+        document.title="React Tutorial"
+    }, [counter]); //cette callback function will be invoked if the state of counter
+    //changed , [] is the array of dependencies in which the function setEffect will be invoked , you can let it empty .
+    //fetch will return a promise object , so it has the functions then(success) , catch , finally
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users', {
+            method:'GET',
+            }
+        ).then((response)=>{return response.json()}).then((data)=>{console.log(data)})
+    }, []);
 return(
     <div>
-<form onSubmit={(e)=>{
-    e.preventDefault();
-    const newUser={
-        id:counter,
-        username,
-        email,
-    };
-    setCounter((currentCounter)=>currentCounter+1);
-    setUsers((currentState)=>[...currentState,newUser]);
-}}>
-        <label htmlFor={"username"}>Username : </label>
-        <input name={"username"} id={"username"} value={username}
-               onChange={(e)=>{
-                   setUsername(e.target.value);
-               }}
-
-        /><br/>
-
-        <label htmlFor={"email"}>Email : </label>
-        <input name={"email"} id={"email"} value={email}
-               onChange={(e)=>{
-                   setEmail(e.target.value);
-               }}
-        /><br/>
-        <button
-
-        >Add User</button></form>
-        {users.map((user) => (
-            <UserProfile key={user.id} user={user} setUsers={setUsers}/>
-        ))}
+        <div>
+            You clicked the button {counter} times
+        </div>
+       <button
+       onClick={()=>setCounter((currentCounterState)=>currentCounterState+1)}
+       >Click Me</button>
     </div>
-
 
 )
 }
